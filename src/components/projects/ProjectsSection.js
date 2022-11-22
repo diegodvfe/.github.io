04e4636fea9347cb0fdc79e369b4216b69c1c67e
diegodvfe <diego.lopez.19@hotmail.es>
2022-11-21 17:20:55 -0900

    done

--- src/components/projects/ProjectsSection.js
+++ src/components/projects/ProjectsSection.js
@@ -51,6 +51,8 @@ const ProjectsSection = props => {
                     <ParagraphLight>
                         <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
                             <iframe width="560" height="315" src="https://www.youtube.com/watch?v=4_um8nXBkVE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}></iframe>
+                          <p>Link to check our project  
+                          <a href="https://www.youtube.com/watch?v=4_um8nXBkVE">Youtube</a></p>
                         </div>
                     </ParagraphLight>
                 </ProjectLeft> }
